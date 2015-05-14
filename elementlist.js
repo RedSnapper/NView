4ab4d43feed013fa1226b983bc3efb04c0b5a414
akis <akis@redsnapper.net>
2015-05-14 17:45:47 +0100

    publish: Added docs script to push to gh-pages
    generated from commit bb15d0be075a4646b27c2dd2c977c8cc8165734a

--- elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Dict"],["c","Export"],["c","Form"],["c","NView"],["c","Session"],["c","Settings"],["c","Singleton"],["c","SIO"],["c","SioForgot"],["c","SioReg"],["c","SIOResetPW"],["c","SIOSetEmail"],["c","SIOSetPW"],["c","SioSignIn"],["c","SioSignOut"]];
