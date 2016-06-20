abca8cd7fd09ac1500e3d7403f843c54b64b2d44
Param Dhaliwal <param@redsnapper.net>
2016-06-20 10:59:06 +0100

    Added tests for NViewLogger

---
+++ tests/src/Log/NViewLoggerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+class NViewLoggerTest extends PHPUnit_Framework_TestCase {
+
+	public function testPushName() {
+		$logger = new NViewLogger('Foo');
+		$logger->pushName('Bar');
+		$this->assertEquals('Bar',$logger->getName(),'->pushName() changes the channel name');
+		$logger->pushName('Baz');
+		$this->assertEquals('Baz',$logger->getName(),'->pushName() changes the channel name');
+	}
+
+	public function testPopName() {
+		$logger = new NViewLogger('Foo');
+		$logger->pushName('Bar');
+		$logger->pushName('Baz');
+		$logger->popName();
+		$this->assertEquals('Bar',$logger->getName(),'->popName() pops the channel name');
+		$logger->popName();
+		$logger->popName();
+		$this->assertEquals('Foo',$logger->getName(),'->popName() pops the channel name unless there are no more channels');
+	}
+}
