32480bacc1858e835f8a164828250d50bd623639
Ben Griffin <ben@redsnapper.net>
2016-09-20 15:31:47 +0100

    SioCaptcha

---
+++ src/sio/SioCaptcha.php
@@ -0,0 +1,65 @@
+<?php
+
+class SioCaptcha {
+	use Form {
+		commit as trait_commit;
+	}
+	public static $siteKey = null;
+	public static $secret = null;
+
+	public function __toString() {
+		return "SioCaptcha";
+	}
+
+	public static function sig() {
+		return "botFilter";
+	}
+
+	function __construct($debug = false) {
+		$this->table = "none";
+		$v = new NView("siocaptcha.ixml");
+		Dict::set(array("errors_captcha_missing-input-response" => "You must show us that you are a human."), "en");
+		$this->iniForm(null, null, false, null, $debug);
+		$this->setfld('captcha', '0');
+	}
+
+	public function func() {
+		$this->valCaptcha('captcha');
+	}
+
+	public function validate() {
+		$this->valCaptcha('captcha');
+	}
+
+	public function populate() {
+		$this->view->set("//*[@data-sitekey]/@data-sitekey", static::$siteKey);
+	}
+
+	public function commit() {
+		return true; //always do nothing in a commit. we are merely validating.
+	}
+
+	public function prefilter() {
+		if (isset($_POST['g-recaptcha-response'])) {
+			$this->fields['captcha'][] = $_POST['g-recaptcha-response'];
+		}
+	}
+
+	private function valCaptcha($name = 'captcha') {
+		if (isset($this->fields[$name][0])) {
+			$response = $this->fields[$name][0];
+			$recaptcha = new \ReCaptcha\ReCaptcha(static::$secret);
+			$resp = $recaptcha->verify($response);
+			if (!$resp->isSuccess()) {
+				$errors = $resp->getErrorCodes();
+				foreach ($errors as $k) {
+					$this->seterr($name, Dict::get("errors_captcha_$k"));
+					$this->valid = false;
+				}
+			}
+		} else {
+			$this->seterr($name, Dict::get('errors_captcha_not_submitted'));
+			$this->valid = false;
+		}
+	}
+}
