6b0079d684619f704aa5463293c6b38d6e2c92db
Param Dhaliwal <paramdhal@gmail.com>
2016-10-21 10:59:49 +0100

    Added registry to Session

--- src/Session.php
+++ src/Session.php
@@ -9,143 +9,83 @@ mb_internal_encoding('UTF-8');
  * We are currently ignoring the following php.ini session.* configuration settings.
  * cf. http://php.net/manual/en/session.configuration.php
  */
-class Session extends Singleton implements \SessionHandlerInterface {
+class Session extends Singleton{
 
-private static $tom=null; 				//timeout minutes
-private static $session=null;
-private static $sqlsess=null;
-private static $apache_cookie=null;		//This is the session cookie or '[new session]'- used for session-only variables.
+	private static $tom=null; 				//timeout minutes
+	private static $session=null;
+	private static $sqlsess=null;
+	private static $apache_cookie=null;		//This is the session cookie or '[new session]'- used for session-only variables.
+	private static $registry;
 
-/*
--SessionHandlerInterface::open — Initialize session
--SessionHandlerInterface::close — Close the session
--SessionHandlerInterface::destroy — Destroy a session
--SessionHandlerInterface::gc — Cleanup old sessions
--SessionHandlerInterface::read — Read session data
--SessionHandlerInterface::write — Write session data
-*/
 
-/*
- * SessionHandlerInterface::open — Initialize session
- * We currently do not use the save_path or the name here.
- * As this is not a static, we have already done the initialise in the constructor.
- */
-public function open($save_path,$name) {
-	return true;
-}
-
-/*
- * SessionHandlerInterface::close — Close the session
- * returns bool. We do nothing for this.
- */
-public function close() {
-	return true;
-}
-
-/*
- * SessionHandlerInterface::close — Close the session
- * returns bool.
- */
-public function destroy($session_id) {
-	$sess = static::$sqlsess;
-	Settings::$sql->query("delete from sio_session where id='$sess'");
-	Settings::$sql->query("delete from sio_sessiondata where sid='$sess'");
-	return true;
-}
-
-/*
- * SessionHandlerInterface::gc — Garbage collection (remove stale sessions)
- * returns bool. $maxlifetime is measured in seconds.
- */
-public function gc( $maxlifetime ) {
-	Settings::$sql->query("delete from sio_session where TIMESTAMPADD(SECOND," . $maxlifetime . ",ts) < CURRENT_TIMESTAMP");
-	return true;
-}
-
-/*
- * SessionHandlerInterface::write —
- * write session data called by session_write_close() so if you are using _SESSION use it, but before closing mysql.
- * returns string (serialised assoc. array). we ignore session_id.
- */
-public function read( $session_id ) {
-	$retval=NULL;
-	$sess = static::$sqlsess;
-	$qry =  "select value from sio_sessiondata where sid='$sess' and name='__SESSION'";
-	if ($rx = Settings::$sql->query($qry)) {
-		$retval = $rx->fetch_row()[0];
-		$rx->close();
-	}
-	return true;
-}
-
-/*
- * SessionHandlerInterface::write —
- * write session data called by session_write_close() so if you are using _SESSION use it, but before closing mysql.
- * returns bool. we ignore session_id. the data is stored only on change.
- */
-public function write( $session_id , $data ) {
-	Settings::esc($data);
-	Settings::$sql->query("replace into sio_sessiondata (sid,name,value) values ('" . static::$sqlsess . "','__SESSION',{$data})");
-	return true;
-}
-
-/*
- * set timeout (in minutes)
- * this is traditionally set using a php.ini value session.gc_lifetime
- */
+	/*
+	 * set timeout (in minutes)
+	 * this is traditionally set using a php.ini value session.gc_lifetime
+	 */
 	public static function setto($tom_p=1440) {
 		static::$tom = Settings::$sql->escape_string($tom_p);
 		Settings::$sql->query("delete from sio_session where TIMESTAMPADD(MINUTE," . static::$tom . ",ts) < CURRENT_TIMESTAMP");
 	}
 
-/**
- * has (with no name = check for session) - otherwise, check for session variable
- */
-	public static function has($nam=NULL) {
-		$retval = false;
+	/**
+	 * has (with no name = check for session) - otherwise, check for session variable
+	 */
+	public static function has($name=NULL) {
+		$retVal = false;
 		if (!empty(static::$session)) {
-			if (!empty($nam)) {
-				$sqlname = Settings::$sql->escape_string($nam);
-				$query= "select count(sid) as found from sio_sessiondata where sid='" . static::$sqlsess . "' and name='" . $sqlname . "' and (session is NULL or session='".static::$apache_cookie."' or session='_NEW')";
-				if ($x = Settings::$sql->query($query)) {
-					if (strcmp($x->fetch_row()[0],"1") === 0) {
-						$retval=true;
-					}
-					$x->close();
-				}
+			if (!empty($name)) {
+				$registry = static::getRegistry();
+				$retVal = isset($registry[$name]);
 			} else {
-				$retval=true;
+				$retVal=true;
 			}
 		}
-		return $retval;
+		return $retVal;
 	}
 
-	public static function get($nam=NULL) {
-		$retval = false;
+	public static function get($name=NULL) {
+		$retVal = false;
 		if (!empty(static::$session)) {
-			if(!is_null($nam)) {
-				$sqlname = Settings::$sql->escape_string($nam);
-				if ($rx = Settings::$sql->query("select value from sio_sessiondata where sid='" . static::$sqlsess . "' and name='" . $sqlname . "' and (session is NULL or session='".static::$apache_cookie."' or session='_NEW')")) {
-					$retval = $rx->fetch_row()[0];
-					$rx->close();
+			if(!is_null($name)) {
+				$registry = static::getRegistry();
+				if(static::has($name)){
+					$retVal = $registry[$name];
 				}
 			} else {
-				$retval = static::$session;
+				$retVal = static::$session;
 			}
 		}
-		return $retval;
+		return $retVal;
 	}
 
-	public static function set($nam=NULL,$val=NULL,$session_only=false) {
-		$retval = false;
-		if (!empty(static::$session) && !is_null($nam) ) {
-			$sqlnam = Settings::$sql->escape_string($nam);
+	public static function set($name=NULL, $val=NULL, $session_only=false) {
+		$retVal = false;
+		if (!empty(static::$session) && !is_null($name) ) {
+			$sqlnam = Settings::$sql->escape_string($name);
 			$sonly = $session_only ? "'".static::$apache_cookie."'" : "NULL";
 			$value = is_null($val) ? "NULL" : "'".Settings::$sql->escape_string($val)."'";
 			Settings::$sql->query("replace into sio_sessiondata (sid,name,value,session) values ('" . static::$sqlsess . "','{$sqlnam}',{$value},{$sonly})");
+			static::$registry[$name] = $val;
 		}
-		return $retval;
+		return $retVal;
 	}
+
+	private static function getRegistry(){
+		if(is_null(static::$registry)){
+			static::$registry = [];
+			$statement = Settings::$sql->prepare("select name,value from sio_sessiondata where sid=? and (session is NULL or session=? or session='_NEW')");
+			$statement->bind_param("ss",static::$sqlsess,static::$apache_cookie);
+			$statement->bind_result($name, $value);
+			$statement->execute();
+			while($statement->fetch()) {
+				static::$registry[$name] = $value;
+			}
+
+		}
+
+		return static::$registry;
+	}
+
 
 	public static function del($nam=NULL) {
 		if (!empty(static::$session)) {
@@ -153,17 +93,19 @@ public function write( $session_id , $data ) {
 				$sqlname = Settings::$sql->escape_string($nam);
 				$query="delete from sio_sessiondata where sid='" . static::$sqlsess . "' and name='" . $sqlname . "'";
 				Settings::$sql->query($query);
+				unset(static::$registry[$nam]);
 			} else {
 				Settings::$sql->query("delete from sio_session where id='" . static::$sqlsess . "'");
 				Settings::$sql->query("delete from sio_sessiondata where sid='" . static::$sqlsess . "'");
+				static::resetRegistry();
 			}
 		}
 	}
 
-/**
- * 'fresh'
- * Returns a boolean. Tests to see if the current session is a new session, or a recovered session..
- */
+	/**
+	 * 'fresh'
+	 * Returns a boolean. Tests to see if the current session is a new session, or a recovered session..
+	 */
 	public static function fresh() {
 		$retval = false;
 		if( (!isset($_COOKIE["session"])) || (static::$session === $_COOKIE["session"])) {
@@ -172,15 +114,16 @@ public function write( $session_id , $data ) {
 		return $retval;
 	}
 
-/**
- * '__construct'
- * Set / manage the session cookie and it's correlating data-record.
- */
+	/**
+	 * '__construct'
+	 * Set / manage the session cookie and it's correlating data-record.
+	 */
 	protected function __construct() {
-		//$this->start(false);
+
 	}
 
 	public static function start($override = false){
+		static::resetRegistry();
 		if(isset($_COOKIE["session"])) {
 			static::$apache_cookie = Settings::$sql->escape_string($_COOKIE["session"]);
 		} else {
@@ -247,6 +190,10 @@ public function write( $session_id , $data ) {
 			static::$apache_cookie = Settings::$sql->escape_string($_COOKIE["session"]);
 			Settings::$sql->query("update sio_sessiondata set session='".static::$apache_cookie."' where sid='".static::$sqlsess."' and session='_NEW'");
 		}
+	}
+
+	private static function resetRegistry(){
+		static::$registry = null;
 	}
 
 
