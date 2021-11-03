cfa989bb3b1f0ceae8bc2bacbdf9e526cef3c307
Param Dhaliwal <paramdhal@gmail.com>
2021-11-03 14:22:18 +0000

    __wakeup now needs to have public visibility

--- src/Singleton.php
+++ src/Singleton.php
@@ -16,5 +16,5 @@ abstract class Singleton {
     }
     protected function __construct() {}
     private function __clone() {}
-    private function __wakeup() {}
+    public function __wakeup() {}
 }
